fe932b7eb0e411f8d4ad2b251196b4a1a724242e
Jotirmoy <jotirmoybhowmik1976@gmail.com>
2025-04-08 16:31:44 +0600

    sitemap

--- app/sitemap.js
+++
@@ -1,8 +0,0 @@
-export default function sitemap() {
-    return [
-      {
-        url: "https://bismillahweldingghor.vercel.app", // Your only page
-        lastModified: new Date(),
-      },
-    ];
-  }
